24500d4bc35ea54e83fca726484199c4290ac3e4
weilyuwang <weilyuwang@gmail.com>
2021-12-17 17:16:53 -0500

    init

---
+++ celeritas.go
@@ -0,0 +1,34 @@
+package celeritas
+
+const version = "1.0.0"
+
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+func (c *Celeritas) New(rootPath string) error {
+	pathConfig := initPaths{
+		rootPath:    rootPath,
+		folderNames: []string{"handlers", "migrations", "views", "data", "public", "tmp", "logs", "middleware"},
+	}
+
+	err := c.Init(pathConfig)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) Init(p initPaths) error {
+	root := p.rootPath
+	for _, path := range p.folderNames {
+		// create folder if it doesn't exist
+		err := c.CreateDirIfNotExist(root + "/" + path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
