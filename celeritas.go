3aaf9a7554fcce7314b41e95865639020f13dce3
weilyuwang <weilyuwang@gmail.com>
2021-12-18 17:26:47 -0500

    create logs

--- celeritas.go
+++ celeritas.go
@@ -3,14 +3,20 @@ package celeritas
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
 )
 
 const version = "1.0.0"
 
 type Celeritas struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+	RootPath string
 }
 
 func (c *Celeritas) New(rootPath string) error {
@@ -35,6 +41,13 @@ func (c *Celeritas) New(rootPath string) error {
 		return err
 	}
 
+	// create loggers
+	infoLog, errorLog := c.startLoggers()
+	c.InfoLog = infoLog
+	c.ErrorLog = errorLog
+	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	c.Version = version
+
 	return nil
 }
 
@@ -56,4 +69,14 @@ func (c *Celeritas) checkDotEnv(path string) error {
 		return err
 	}
 	return nil
+}
+
+func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module celeritas
 
 go 1.17
 
-require github.com/joho/godotenv v1.4.0 // indirect
+require github.com/joho/godotenv v1.4.0
