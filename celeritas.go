bb4c8a7f72db30e9b07960d8c2d56171f3d86e0a
weilyuwang <weilyuwang@gmail.com>
2021-12-18 17:11:55 -0500

    create and read the .env file

--- celeritas.go
+++ celeritas.go
@@ -1,5 +1,10 @@
 package celeritas
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
 const version = "1.0.0"
 
 type Celeritas struct {
@@ -18,6 +23,18 @@ func (c *Celeritas) New(rootPath string) error {
 	if err != nil {
 		return err
 	}
+
+	err = c.checkDotEnv(rootPath)
+	if err != nil {
+		return err
+	}
+
+	// read .env
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -29,6 +46,14 @@ func (c *Celeritas) Init(p initPaths) error {
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExists(fmt.Sprintf("%s/.env", path))
+	if err != nil {
+		return err
 	}
 	return nil
 }
--- helpers.go
+++ helpers.go
@@ -12,3 +12,18 @@ func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	}
 	return nil
 }
+
+func (c *Celeritas) CreateFileIfNotExists(path string) error {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+
+		defer func(file *os.File) {
+			_ = file.Close()
+		}(file)
+	}
+	return nil
+}
